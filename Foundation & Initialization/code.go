12cf24b671c2ef9f200a1d827624459d8caad675
Mohit Yadav <ymohit799057@gmail.com>
2026-07-27 11:43:39 +0530

    Update code.go

--- Foundation & Initialization/code.go
+++ Foundation & Initialization/code.go
@@ -1,1 +1,33 @@
+package main
 
+import "fmt"
+
+// 1. Package-level variables are evaluated first.
+// Go determines the order based on dependencies.
+var globalA = initializeA()
+var globalB = initializeB()
+
+func initializeA() int {
+	fmt.Println("1. Allocating globalA")
+	return 10
+}
+
+func initializeB() int {
+	fmt.Println("2. Allocating globalB")
+	return 20
+}
+
+// 2. init() functions run after variable initialization but before main().
+// You can have multiple init() functions in a single file.
+func init() {
+	fmt.Println("3. Running first init()")
+}
+
+func init() {
+	fmt.Println("4. Running second init()")
+}
+
+func main() {
+	// 3. main() is the final step of the boot process.
+	fmt.Println("5. Executing main()")
+}
